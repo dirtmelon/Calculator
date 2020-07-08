6dc220a5e9902e57d0122316f3637c15a41fd608
dirtmelon <0xffdirtmelon@gmail.com>
2020-07-08 20:01:34 +0800

    Initial commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by liguofeng on 2020/7/8.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculator/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by liguofeng on 2020/7/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
